68ddba7112d57e3afe9862006d309dbdf62438de
Tony Critchett <40681613+Condabas@users.noreply.github.com>
2024-06-01 22:35:29 -0700

    Add files via upload

--- bossquest.lua
+++ bossquest.lua
@@ -10,8 +10,8 @@ boss_quest_alliance["Nefarian"] = "\124cffa335ee\124Hitem:19002:0:0:0:0:0:0:0:0\
 boss_quest_alliance["Ossirian the Unscarred"] = {"\124cffa335ee\124Hitem:21220:0:0:0:0:0:0:0:0\124h[Head of Ossirian the Unscarred]\124h\124r"}
 
 boss_quest_horde = {}
-boss_quest_horde["Sawtooth Crocolisk"] = {"\124cffa335ee\124Hitem:22520:0:0:0:0:0:0:0:0\124h[The Phylactery of Kel'Thuzad]\124h\124r","\124cffa335ee\124Hitem:61652:0:0:0:0:0:0:0:0\124h[Claw of Erennius]\124h\124r"} --test subject
-boss_quest_horde["Deathstrike Tarantula"] = {"\124cffa335ee\124Hitem:19003:0:0:0:0:0:0:0:0\124h[Head of Nefarian (Alliance)]\124h\124r"} --test subject
+-- boss_quest_horde["Sawtooth Crocolisk"] = {"\124cffa335ee\124Hitem:22520:0:0:0:0:0:0:0:0\124h[The Phylactery of Kel'Thuzad]\124h\124r","\124cffa335ee\124Hitem:61652:0:0:0:0:0:0:0:0\124h[Claw of Erennius]\124h\124r"} --test subject
+-- boss_quest_horde["Deathstrike Tarantula"] = {"\124cffa335ee\124Hitem:19003:0:0:0:0:0:0:0:0\124h[Head of Nefarian (Alliance)]\124h\124r"} --test subject
 boss_quest_horde["Kel'Thuzad"] = {"\124cffa335ee\124Hitem:22520:0:0:0:0:0:0:0:0\124h[The Phylactery of Kel'Thuzad]\124h\124r"}
 boss_quest_horde["Erennius"] = {"\124cffa335ee\124Hitem:61652:0:0:0:0:0:0:0:0\124h[Claw of Erennius]\124h\124r"}
 boss_quest_horde["Solnius"] = {"\124cffa335ee\124Hitem:61444:0:0:0:0:0:0:0:0\124h[Smoldering Dream Essence]\124h\124r","\124cffa335ee\124Hitem:61215:0:0:0:0:0:0:0:0\124h[Head of Solnius]\124h\124r"} -- Smoldering doesn't always drop
